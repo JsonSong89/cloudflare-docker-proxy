e7118727cc7adcfea81d4d08c4d932528f2ca35d
jsonsong <jsonsong@hotmail.com>
2024-06-18 22:21:29 +0800

    myconfig 1.3

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.jsonsong.win": dockerHub,
+  "quay.jsonsong.win": "https://quay.io",
+  "gcr.jsonsong.win": "https://gcr.io",
+  "k8s-gcr.jsonsong.win": "https://k8s.gcr.io",
+  "k8s.jsonsong.win": "https://registry.k8s.io",
+  "ghcr.jsonsong.win": "https://ghcr.io",
+  "cloudsmith.jsonsong.win": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.jsonsong.win": dockerHub,
 };
 
 function routeByHosts(host) {
